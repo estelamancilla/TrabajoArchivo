b5d7b3f1c6f49451d647015a541327761590c595
unknown <estelamancilla.95@gmail.com>
2016-10-28 00:51:06 -0300

    nueva entrega

--- TPFlores.pas
+++ TPFlores.pas
@@ -8,142 +8,144 @@ type registro = record
                     Petal_Width : real;
                     Species : string;
                 end;
+     tipo = record      
+                    Sepal_Length : real;
+                    Sepal_Width : real;
+                    Petal_Length :real;
+                    Petal_Width : real;
+                    Species : string;
+                    Distancia : real;
+                  end;
 var 
     datos : file of registro;
     M : ARRAY [1..120] of registro ;
     T : ARRAY [1..30] of registro;
     D : ARRAY [1..120] of real;
-    i:integer;
+    i,j:integer;
     r : registro;
-    CANT:integer;
-    TOTAL:Integer;
-    CT1:Integer;
-    CT2:Integer;
-    CT3:Integer;
-    x2:integer;
-    y2:integer;
-    
-    resultado:string;
-    x: real;
-    y: real;
-function distancia (x1,y1,x2,y2:real):Real;
-    begin
-        distancia:= sqrt(power(x1-x2,2)+power(y1-y2,2));
-                  
-    end;
+    C:integer;
+    Tot:Integer;
+    CTver:Integer;
+    CTS:Integer;
+    CTVir:Integer;
+    An:real;
+    Lar:real;
+    S: real;
+    Tip: string;
 
-procedure calcular_diferencia(x: real; y:real);
-var i : integer;
-   
-    begin
-        For i:=1 to 120 do
-            D[i] := distancia(x,y, m[i].Petal_Length, m[i].Petal_Width);
-    end;  
-
-procedure ordenar () ;
-var
-i: integer;
-j: integer;
-aux_d:real;
-aux_m : registro;
-    begin
-
-    aux_d:=0;
-        for i:= 1 to 119 do
+    function distancia (x1,y1,x2,y2:real):Real;
          begin
-         
-           for j := i+1 to 120 do
-            begin
-            
-                aux_d:= D[i];
-                D[i]:= D[j];
-                D[j]:= aux_d;
-                Aux_m:=M[i];
-                M[i]:=M[j];
-                M[j]:= Aux_m;
-                
-            end;
-         end;   
-    end;
+            distancia:= sqrt(power(x1-x2,2)+power(y1-y2,2));
+         end;
 
+    function calcular (x1: real; y1:real):Real;
+        var 
+            aux_d:real;
+            aux_m : registro;
+            CTver: Integer;
+            CTS:Integer;
+            CTVir:Integer;
+        begin
+                CTver:= 0;
+                CTS:=0;
+                CTVir:=0;
+                For i:=1 to 120 do
+                begin
+                    D[i] := distancia(x1,y1, m[i].Petal_Length, m[i].Petal_Width);
+                end;
+                begin  
+                    For i:= 1 to 119 do
+                        begin
+                            for j := i+1 to 120 do
+                                begin
+                                    if D[i] > D[j] then
+                                        begin 
+                                            aux_d:= D[i];
+                                            D[i]:= D[j];
+                                            D[j]:= aux_d;
+                                            Aux_m:=M[i];
+                                            M[i]:=M[j];
+                                            M[j]:= Aux_m;
+                                        end;
+                                end;   
+                        end;
+                end;
+        end;
+        
 begin
-    CT3:=1;
-    CT2:=1;
-    TOTAL:=1;
-    CANT:=1;
-    CT1:=1;
+    CTver:=1;
+    CTS:=1;
+    CTVir:=1;
+    Tot:=1;
+    C:=1;
+    S:=0;
     Assign(datos, 'iris.dat');
     Reset(datos);
-    while not Eof(datos) do
-    begin
-        read(datos, r);
-              
-        if (r.species) = 'setosa' then
-        begin  
-            If CT1 <= k then 
+        while not Eof(datos) do
+        begin
+            read(datos, r);       
+            if (r.species) = 'setosa' then 
+            begin  
+                If CTS <= 10 then 
+                begin
+                    T[tot]:= r;
+                    Tot:= Tot+1;
+                    CTS:= CTS+1;    
+                end
+                else 
+                begin 
+                    M[C]:= r;
+                    C:= C+1; 
+                end; 
+            end;
+        
+            if (r.species) = 'versicolor' then
             begin
-                T[CANT]:= r;
-                CANT:= CANT+1;
-                CT1:= CT1+1;    
-            end
-            else 
-            begin
-                M[TOTAL]:= r;
-                TOTAL:= TOTAL+1; 
+                if CTver<= 10 then
+                begin      
+                    T[Tot]:= r;
+                    Tot:= Tot +1;
+                    CTver:= CTver+1;
+                end
+                else
+                begin
+                    M[C]:= r;
+                    C:= C+1;
+                end;
             end; 
-        end;
-    
 
-        if (r.species) = 'versicolor' then
-        begin
-            if ct2<= k then
-            begin      
-                T[CANT]:= r;
-                CANT:=CANT+1;
-                CT2:= CT2+1;
-            end
-            else
+            if (r.species) = 'virginica' then
             begin
-                M[TOTAL]:= r;
-                TOTAL:= TOTAL+1;
+                if CTVir<= 10 then
+                begin
+                    T[Tot]:= r;
+                    Tot:= Tot+1;
+                    CTVir:= CTVir+1;
+                end
+                else 
+                begin
+                    M[C]:= r;
+                    C:=C+1;  
+                end;
             end;
-        end; 
-
-
-        if (r.species) = 'virginica' then
-        begin
-            if ct3<=k then
-            begin
-                T[CANT]:= r;
-                CANT:=CANT+1;
-                CT3:= CT3+1;
-            end
-            else 
-            begin
-                M[TOTAL]:= r;
-                TOTAL:=TOTAL+1;  
-            end;
-        end;
-    end;//end while
-    
-
+        end;//end while
     close(datos);
 
-    for i:= 1 to 30 do
-       begin
-          writeln(i, ' ', t[i].Species, t[i].Petal_Length, t[i].Petal_Width);
-          readkey ();
-       end;
+    Writeln ('ingrese el largo');
+    ReadLn (Lar);
+    WriteLn ('Ingrese ancho');
+    ReadLn (An);
+        Tip := tipo (Lar,An);
+        writeln (Tip);
 
-{for i:= 1 to 120 do 
-    begin
-        readln(x);
-        readln(y);
-        resultado:=calcular_diferencia(x,y);
-        writeln(resultado);
-        readkey();
+    For i := 1 to 30 do
+        begin
+            if  tipo(T[i].Petal_Length, T[i].Petal_Width) = t[i].Species then
+          begin
+             S:= S+1;
+          end;
     end;
- 
- }  
- 
+    writeln((S/30*100):3:2,'%');
+    Readkey();
+
 end.
